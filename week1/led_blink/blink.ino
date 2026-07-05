62d3e35f340cf2767e705692dad9b87b335c0412
saharsh136 <saharshmishra37@gmail.com>
2026-07-05 15:37:33 +0530

    feat: add serial blink counter

--- week1/led_blink/blink.ino
+++ week1/led_blink/blink.ino
@@ -1,13 +1,9 @@
-/*
- * Author: Saharsh Mishra
- * Date: 05-07-2026
- * Description: Basic LED Blink
- */
-
 const int LED = 13;
+int blinkCount = 0;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -16,4 +12,8 @@ void loop() {
 
   digitalWrite(LED, LOW);
   delay(500);
+
+  blinkCount++;
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount);
 }
