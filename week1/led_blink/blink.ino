45ab0b571d384535384886ec17bd4f19067dccd9
saharsh136 <saharshmishra37@gmail.com>
2026-07-05 15:37:13 +0530

    feat: add basic LED blink program

--- week1/led_blink/blink.ino
+++ week1/led_blink/blink.ino
@@ -1,14 +1,19 @@
-const int LED_PIN = 4;
-const int BLINK_DELAY = 1000; // Delay in milliseconds
+/*
+ * Author: Saharsh Mishra
+ * Date: 05-07-2026
+ * Description: Basic LED Blink
+ */
+
+const int LED = 13;
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH); // Turn LED ON
-    delay(BLINK_DELAY);
+  digitalWrite(LED, HIGH);
+  delay(500);
 
-    digitalWrite(LED_PIN, LOW);  // Turn LED OFF
-    delay(BLINK_DELAY);
+  digitalWrite(LED, LOW);
+  delay(500);
 }
