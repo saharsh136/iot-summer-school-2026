152d11a882e4f2a5b01098d617f080f7ab9eb35e
Saharsh Mishra <saharshmishra37@gmail.com>
2026-07-05 17:23:08 +0530

    feat:  add Sol chord substitute

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -1,79 +1,41 @@
 const int buzzer = 8;
 
-const int buttonDo = 2;
-const int buttonRe = 3;
-const int buttonMi = 4;
-const int buttonFa = 5;
-const int modeButton = 6;
-
-bool majorMode = true;
-bool lastModeState = HIGH;
-
-// Major Scale
-int majorNotes[] = {262, 294, 330, 349};
-
-// Minor Scale
-int minorNotes[] = {262, 294, 311, 349};
+const int doBtn = 2;
+const int reBtn = 3;
+const int miBtn = 4;
+const int faBtn = 5;
 
 void setup() {
-  pinMode(buttonDo, INPUT_PULLUP);
-  pinMode(buttonRe, INPUT_PULLUP);
-  pinMode(buttonMi, INPUT_PULLUP);
-  pinMode(buttonFa, INPUT_PULLUP);
-  pinMode(modeButton, INPUT_PULLUP);
-
-  Serial.begin(9600);
+  pinMode(doBtn, INPUT_PULLUP);
+  pinMode(reBtn, INPUT_PULLUP);
+  pinMode(miBtn, INPUT_PULLUP);
+  pinMode(faBtn, INPUT_PULLUP);
 }
 
 void loop() {
 
-  // ---------- Mode Toggle ----------
-  bool currentModeState = digitalRead(modeButton);
+  bool d = digitalRead(doBtn) == LOW;
+  bool r = digitalRead(reBtn) == LOW;
+  bool m = digitalRead(miBtn) == LOW;
+  bool f = digitalRead(faBtn) == LOW;
 
-  if (lastModeState == HIGH && currentModeState == LOW) {
-    majorMode = !majorMode;
+  int pressed = d + r + m + f;
 
-    if (majorMode)
-      Serial.println("Major Mode");
-    else
-      Serial.println("Minor Mode");
-
-    delay(200);   // Debounce
+  if (pressed >= 2) {
+    tone(buzzer, 392);
   }
 
-  lastModeState = currentModeState;
+  else if (d)
+    tone(buzzer, 262);
 
-  bool doPressed = digitalRead(buttonDo) == LOW;
-  bool rePressed = digitalRead(buttonRe) == LOW;
-  bool miPressed = digitalRead(buttonMi) == LOW;
-  bool faPressed = digitalRead(buttonFa) == LOW;
+  else if (r)
+    tone(buzzer, 294);
 
-  int pressedButtons = doPressed + rePressed + miPressed + faPressed;
+  else if (m)
+    tone(buzzer, 330);
 
-  // If two or more buttons are pressed
-  if (pressedButtons >= 2) {
-    tone(buzzer, 392);   // Sol
-    return;
-  }
-
-  int *notes;
-
-  if (majorMode)
-    notes = majorNotes;
-  else
-    notes = minorNotes;
-
-  if (doPressed)
-    tone(buzzer, notes[0]);
-
-  else if (rePressed)
-    tone(buzzer, notes[1]);
-
-  else if (miPressed)
-    tone(buzzer, notes[2]);
-
-  else if (faPressed)
-    tone(buzzer, notes[3]);
+  else if (f)
+    tone(buzzer, 349);
 
   else
     noTone(buzzer);
